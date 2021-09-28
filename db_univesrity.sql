65e4aa49eb0d29cf06645797d0b964a09292e4a4
Valentina-Fr <v_frasca@virgilio.it>
2021-09-28 15:41:55 +0200

    Exercise 4

--- db_univesrity.sql
+++ db_univesrity.sql
@@ -12,6 +12,13 @@ WHERE `cfu` > 10;
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+
 SELECT * 
 FROM `students`
 WHERE YEAR(`date_of_birth`) > '1991';
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+
+SELECT * 
+FROM `courses`
+WHERE `period` = 'I semestre' AND `year` = 1;
