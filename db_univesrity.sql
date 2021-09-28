17cb56bd9c0c2bb13df7962cd3904d7579fadeb9
Valentina-Fr <v_frasca@virgilio.it>
2021-09-28 16:00:45 +0200

    Exercise 8

--- db_univesrity.sql
+++ db_univesrity.sql
@@ -40,5 +40,12 @@ WHERE `level` = 'magistrale';
 SELECT COUNT(*)
 FROM `departments`;
 
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT COUNT(*)
+FROM `teachers`
+WHERE `phone` IS NULL;
+
+
 
 
