9fc8b221ffe038f413c09bedff1347a3d7ddcdf9
Valentina-Fr <v_frasca@virgilio.it>
2021-09-29 15:39:28 +0200

    Ex 3

--- join_query.sql
+++ join_query.sql
@@ -12,3 +12,13 @@ JOIN `degrees`
 ON `departments`. `id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `teachers`.`id`, `teachers`.`name`, `teachers`.`surname`, `courses`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`. `teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44;
+
+
